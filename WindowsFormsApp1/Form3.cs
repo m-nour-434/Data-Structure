e6ac0b92608485fd2fe04fe48b1391fb05470907
MOHAMAD NOUR ALMASRI <032390142@ogr.uludag.edu.tr>
2025-05-13 00:15:06 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form3.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace WindowsFormsApp1
+{
+    public partial class PostEkleForm : Form
+    {
+
+        public PostEkleForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PostEkleClosebt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PostEklebtn_Click(object sender, EventArgs e)
+        {
+            string Title =Titletxt.Text;
+            string Content = Contenttxt.Text;
+            string name = User.user.Users;
+            if (!string.IsNullOrEmpty(Title) && !string.IsNullOrEmpty(Content))
+            {
+                FeedManager.feedmanager.AddPost(Title, Content,name); 
+                this.Close();  
+            }
+            else
+            {
+                MessageBox.Show("Lütfen başlık ve içerik giriniz.");
+            }
+
+            
+        }
+
+
+    }
+
+
+
+}
---
+++ WindowsFormsApp1/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+public class User
+{
+    public string FullName { get; set; }
+    public string Users { get; set; }
+    public string Password { get; set; }
+    public static User user = new User();
+}
