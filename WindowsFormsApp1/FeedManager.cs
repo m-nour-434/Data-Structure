78886f98b6a9e2de81da1160748181f6a4f0b5a0
ABDULRUHMANKATIA <032290149@ogr.uludag.edu.tr>
2025-05-13 12:16:29 +0300

    Add files via upload

---
+++ WindowsFormsApp1/FeedManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using WindowsFormsApp1;
+
+public class FeedManager
+{
+    private Post head;
+    private Post tail;
+    private Post current;
+    private int counter = 1;
+    public static  FeedManager feedmanager = new FeedManager();
+    public static ArrayList ar = new ArrayList();
+
+    public void AddPost(string title, string content,string name)
+    {
+        
+
+        Post newPost = new Post
+        {
+            Title = title,
+            Content = content,
+            Name = name,
+            Timestamp = DateTime.Now
+        };
+
+        if (head == null)
+        {
+            head = tail = current = newPost;
+        }
+        else
+        {
+            tail.Next = newPost;
+            newPost.Prev = tail;
+            tail = newPost;
+        }
+        foreach (var item1 in UserData.Users.Keys)
+        {
+            WindowsFormsApp2.stack.mystack.Push(string.Format("{0} Post ekledi", item1.ToString()));
+        }
+    }
+
+    public Post GetCurrent() => current;
+
+    public void MoveNext() { if (current?.Next != null) current = current.Next; }
+
+    public void MovePrevious() { if (current?.Prev != null) current = current.Prev; }
+
+    public void DeleteCurrent()
+    {
+        if (current == null) return;
+
+        if (current.Prev != null) current.Prev.Next = current.Next;
+        else head = current.Next;
+
+        if (current.Next != null) current.Next.Prev = current.Prev;
+        else tail = current.Prev;
+
+        current = current.Next ?? current.Prev;
+    }
+}
---
+++ WindowsFormsApp1/Form7.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.goster_btn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Simplified Arabic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 39;
+            this.listBox1.Location = new System.Drawing.Point(166, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // goster_btn
+            // 
+            this.goster_btn.Location = new System.Drawing.Point(37, 369);
+            this.goster_btn.Name = "goster_btn";
+            this.goster_btn.Size = new System.Drawing.Size(178, 50);
+            this.goster_btn.TabIndex = 1;
+            this.goster_btn.Text = "Bildirimler Goster";
+            this.goster_btn.UseVisualStyleBackColor = true;
+            this.goster_btn.Click += new System.EventHandler(this.goster_btn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 369);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Geri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.goster_btn);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form7";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form7";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button goster_btn;
+        private System.Windows.Forms.Button button2;
+    }
+}
